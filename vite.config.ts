84a71b1fd3146f457bc55011bd9144460a8194e8
Websprint <websprintt@gmail.com>
2026-05-10 04:05:03 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/JCH-Impact',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
